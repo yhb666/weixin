14c94fad3d020d2f264023e14b0a619a0b30fc30
yhb666 <957731440@qq.com>
2017-09-07 11:18:06 +0800

    杨红波

--- js/weixin.js
+++ js/weixin.js
@@ -1,26 +1,58 @@
-window.onload = function() {
-    var pack = {
-        init: function() {
+ var con = document.getElementsByClassName("con");
+ var orign = document.getElementsByClassName("div-img");
+ var flag = true;
+ var speed = 0;
+ var num = 0;
+ var timer = "";
+ var renderData = "";
+ var that = this;
+ for (var i = 0; i < con.length; i++) {
+     con[i].onclick = function() {
+         var a = this.children[1];
+         var img1 = this.children[0];
+         var imgs = img1.children;
+         if (flag == false) {
+             a.pause();
+             clearInterval(timer);
+             for (var j = 0; j < imgs.length; j++) {
+                 imgs[j].className = "";
+             }
+             imgs[0].className = "active";
+             flag = true;
+             console.log("暂停");
+         } else {
+             // console.log(this.children[1]);
+             for (var i = 0; i < con.length; i++) {
+                 audio[i].pause();
+             }
+             timer = setInterval(function() {
+                 for (var i = 0; i < audio.length; i++) {
+                     if (audio[i].ended) {
+                         clearInterval(timer)
+                         for (var j = 0; j < imgs.length; j++) {
 
+                             imgs[j].className = "";
+                         }
+                         imgs[num].className = "active";
+                     }
+                 }
 
+                 num++;
+                 if (num > imgs.length - 1) {
+                     num = 0;
+                 }
 
+                 for (var j = 0; j < imgs.length; j++) {
 
-        },
-        initDom: function() {
+                     imgs[j].className = "";
+                 }
+                 imgs[num].className = "active";
 
-
-
-
-        }
-
-
-
-
-
-    }
-
-
-
-
-
-}
+             }, 500)
+             a.play();
+             a.currentTime = 0;
+             flag = false;
+             console.log("播放")
+         }
+     }
+ }
